637d5eae4cfa6c7d7d3a05637c70ecf1666b6b5a
xiechao06 <xiechoa06@gmail.com>
2016-06-01 10:03:32 +0800

    fix #6

--- src/region_investigation_admin/org.clj
+++ src/region_investigation_admin/org.clj
@@ -6,28 +6,32 @@
     [region-investigation-admin.config :refer [upload-dir]]
     [clojure.java.io :as io]))
 
+(defn load-orgs []
+  (as-> (io/file upload-dir "orgs.json") file (if (.exists file) (parse-string (slurp file)) []))
+  )
+
 (defroutes org-routes
            (context "/org" []
-                    (POST "/" {params :body}
-                      (let [file (io/file upload-dir "orgs.json")
-                            orgs (if (.exists file) (parse-string (slurp file)) [])
-                            code (params "code")
-                            name (params "name")]
-                        (if (some (fn [org] (= (org "code") code)) orgs)
-                          (response/status (response/response {
-                                                               :code (str "机构代码" code "已经存在")
-                                                               }) 403)
+             (GET "/list" [] (response/response {:data (load-orgs)}))
+             (POST "/object" {params :body}
+               (let [orgs (load-orgs)
+                     code (params "code")
+                     name (params "name")]
+                 (if (some (fn [org] (= (org "code") code)) orgs)
+                   (response/status (response/response {
+                                                        :code (str "机构代码" code "已经存在")
+                                                        }) 403)
 
-                          (response/response
-                            (do
-                              (spit (io/file upload-dir "orgs.json") (generate-string
-                                                                       (concat orgs [{
-                                                                                      :code code
-                                                                                      :name name
-                                                                                      }])))
-                              {})
+                   (response/response
+                     (do
+                       (spit (io/file upload-dir "orgs.json") (generate-string
+                                                                (concat orgs [{
+                                                                               :code code
+                                                                               :name name
+                                                                               }])))
+                       {})
 
-                            )
-                          )
-                        )
-                      )))
+                     )
+                   )
+                 )
+               )))
--- src/region_investigation_admin/poi_type.clj
+++ src/region_investigation_admin/poi_type.clj
@@ -44,5 +44,3 @@
                  "content-disposition" (str "attachment; filename=\"" name_ "\"")))
              )
   )
-
-
